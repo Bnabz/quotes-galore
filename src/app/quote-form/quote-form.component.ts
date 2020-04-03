966b5ab968526969b12f8587026bc40984be5a8e
Nabz <bnnabiswa@gmail.com>
2020-04-03 18:00:27 +0300

    add formgroup and form control

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter  } from '@angular/core';
 import { Quote } from '../quote';
 import {FormGroup,FormControl,Validators} from '@angular/forms';
 
@@ -8,6 +8,20 @@ import {FormGroup,FormControl,Validators} from '@angular/forms';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+
+  newQuote = new Quote("","",new Date());
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  quoteForm = new FormGroup({
+    username: new FormControl("",Validators.required),
+    author: new FormControl("",Validators.required),
+    quotation: new FormControl("",Validators.required)
+
+  });
+
+   submitQuote(){
+     this.addQuote.emit(this.newQuote);
+   }
 
   constructor() { }
 
