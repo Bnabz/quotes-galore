69d8d3b157a4d81683dbe59a03f6a9af15cbcbd6
Nabz <bnnabiswa@gmail.com>
2020-04-03 07:13:21 +0300

    add likes property

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   showDetails: boolean;
-constructor(public username: string,public author: string , public quotation: string, public submitDate: Date){
+constructor(public username: string,public author: string , public quotation: string,public likes: number, public submitDate: Date){
 this.showDetails=false;
 }
 }
