55a862180364a7c3213d331ab6d661a1c2732f4c
Nabz <bnnabiswa@gmail.com>
2020-04-03 14:18:38 +0300

    implement the like count function

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,11 @@
 export class Quote {
   showDetails: boolean;
+  popularQuote:boolean;
+
 constructor(public username: string,public author: string , public quotation: string,public likes: number, public submitDate: Date){
 this.showDetails=false;
+this.popularQuote=false;
+
+
 }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,10 +9,55 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
   quotes: Quote[]=[
     new Quote('Brian','George Carlin','"Think of how stupid the average person is, then realize half of them are stupider than that."', 5,new Date()),
-    new Quote ('Stacy','Carl Sagan',"\"We'r made of star stuff.We are a way for the universe to know itself.\"" ,3,new Date()),
-    new Quote('Laureen','Marcus Aurelius',"\"Choose not to be harmed,and you wont feel harmed.Don't feel harmed,and you have not been harmed\"",1,new Date()),
+    new Quote ('Stacy','Carl Sagan',"\"We'r made of star stuff.We are a way for the universe to know itself.\"" ,8,new Date()),
+    new Quote('Laureen','Marcus Aurelius',"\"Choose not to be harmed,and you wont feel harmed.Don't feel harmed,and you have not been harmed\"",3,new Date()),
 
   ];
+  most:number = 0;
+
+
+  /* findMostPopular() {
+      let popular =0;
+      for(let i =0;i<this.quotes.length; i++){
+        if(this.quotes[i].likes>popular){
+          popular=this.quotes[i]
+          return true
+
+      }
+
+
+
+      }
+
+    }*/
+
+    findMostPopular() {
+
+      //let most = 0;
+      for (let i = 0; i < this.quotes.length; i++) {
+          if(this.quotes[i].likes>this.most){
+            this.most= this.quotes[i].likes;
+            /*this.popular=this.quotes[i].likes
+             this.quotes[i].popularQuote = !this.quotes[i].popularQuote;*/
+              //console.log(most)
+          }
+
+
+
+
+      }
+       for (let i = 0; i < this.quotes.length; i++) {
+              if(this.quotes[i].likes===this.most){
+                      console.log(this.quotes[i])
+                  this.quotes[i].popularQuote = true
+              }
+
+        }
+
+
+    }
+
+
 
   toggleDetails(index){
      this.quotes[index].showDetails = !this.quotes[index].showDetails;
@@ -43,6 +88,10 @@ export class QuoteComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
+
+    this.findMostPopular()
+}
+
+
 
 }
